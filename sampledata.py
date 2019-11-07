be954ce0a86441a26d976238e80bd306f6326240
surya narayana <myrala.suri@gmail.com>
2019-11-07 15:00:47 +0530

    Create sampledata.py

---
+++ sampledata.py
@@ -0,0 +1,21 @@
+[{name: 'web', status: 'passed', modules:[{name:'module1', tcs: [], time:'0.6778sec'},{name:'modul2',..},{..}], time:'7.87sec'}
+{name: 'mobile', status: 'passed', modules:[{},{},{}], time:'7.87sec'}
+{name: 'api', status: 'passed', modules:[{},{},{}], time:'7.87sec'}
+]
+
+
+----------------------------------
+
+Module:
+
+modules:
+[{name:'module1', tcs: [], status:'passed', time:'0.6778sec'},
+{name:'modul2',..},
+{..}]
+
+-----------------------------------------------------
+Testcase:
+tc: [{mname: 'modeul1', name:'testrcasw1', status:'failed', duration:'0.657'},
+{mname: 'modeul1', name:'testrcasw1', status:'failed', duration:'0.657'},
+{mname: 'modeul1', name:'testrcasw1', status:'failed', duration:'0.657'}]
+-----------------------------------------------
